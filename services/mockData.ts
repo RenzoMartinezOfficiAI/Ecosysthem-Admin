70f907da4d9246b2476757f16dd8c94c757d9027
lorenzomartinez.official <lorenzomartinez.official@gmail.com>
2025-12-16 05:05:12 +0000

    Changes before Firebase Studio auto-run

--- services/mockData.ts
+++ services/mockData.ts
@@ -1,18 +1,1 @@
-
-import { Member, House, Sponsorship, BedCharge, MemberPayment, SystemError, InventoryItem, AuditLogEntry, WorkOrder } from '../types';
-
-export const MOCK_HOUSES: House[] = [];
-
-export const MOCK_INVENTORY: InventoryItem[] = [];
-
-export const MOCK_WORK_ORDERS: WorkOrder[] = [];
-
-export const MOCK_MEMBERS: Member[] = [];
-
-export const MOCK_SPONSORSHIPS: Sponsorship[] = [];
-
-export const MOCK_SYSTEM_ERRORS: SystemError[] = [];
-
-export const MOCK_TRANSACTIONS: (BedCharge | MemberPayment)[] = [];
-
-export const MOCK_AUDIT_LOGS: AuditLogEntry[] = [];
+```
